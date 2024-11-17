4d968fc8dfef2a815c43e7e7f131de318a04df7a
Samuel Skean <sskean2@uic.edu>
2024-11-16 22:13:52 -0600

    Untested program runner.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
+use instructions::Instruction;
+use registers::RegisterFile;
+
 pub mod registers;
 pub mod instructions;
+
+pub fn run(program: &[Instruction]) {
+    let mut rf = RegisterFile::new();
+    run_against_rf(&mut rf, program);    
+}
+
+fn run_against_rf(rf: &mut RegisterFile, program: &[Instruction]) {
+    let label_indices = program
+        .iter()
+        .enumerate()
+        .filter_map(|(i, instr)| match instr {
+            &Instruction::Label(l) => Some((l, i)),
+            _ => None,
+        }).collect();
+    while rf.program_counter < program.len() {
+        program[rf.program_counter].run(rf, &label_indices);
+    }
+}
